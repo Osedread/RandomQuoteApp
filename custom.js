3f9db0cc435f5e101ae7ca24a4ed2639cacdcc0c
Jose Diaz-Gonzalez <osedread@gmail.com>
2017-03-15 11:46:44 +0000

    Fixed tweet button to display quote and author.

--- custom.js
+++ custom.js
@@ -1,12 +1,10 @@
 var $quoteBox = $('#quoteBox');
-
-
-
+var $quoteBtn = $('#generateQuote');
+var $tweetBtn = $('#tweetQuote');
 
 $(document).ready(function () {
 	
-	var $quoteBtn = $('#generateQuote');
-
+	// Link Event handler with Quote API
 	$quoteBtn.on('click', function() {
 		$.ajax({
 			url: 'http://api.forismatic.com/api/1.0/',
@@ -18,10 +16,13 @@ $(document).ready(function () {
 				$quoteBox.append('<p>' + response.quoteText + '</p>');
 				$quoteBox.append('<cite>- ' + response.quoteAuthor + '</cite>');
 				$quoteBox.fadeIn();
+				// Since we depend on the HREF attribute of the Twitter button to tweet the content through the line '../tweet?text=...' we need to introduce the content into the end of the HREF.
+				$tweetBtn.attr('href', 'https://twitter.com/intent/tweet?text=' + response.quoteText + ' -' + response.quoteAuthor);
 			}
 		});
 	});
-
+	
+	
 
 
 });
